0d3270893e8722e43796b4104cc729f26a6ac058
Victor Maslov <nakilon@gmail.com>
2022-04-07 05:46:54 +0300

    github actions add the INPUT_ prefix

--- Yandex/main.rb
+++ Yandex/main.rb
@@ -1,6 +1,6 @@
-require "pp"
-pp ENV
 Dir[?*].each do |bucket|
   next unless File.directory? bucket
+  ENV.store "AWS_ACCESS_KEY_ID", ENV.delete("INPUT_AWS_ACCESS_KEY_ID")
+  ENV.store "AWS_SECRET_ACCESS_KEY", ENV.delete("INPUT_AWS_SECRET_ACCESS_KEY")
   `rclone -v --auto-confirm sync #{bucket} :s3,provider=Other,endpoint=storage.yandexcloud.net,env_auth:#{bucket}`
 end
